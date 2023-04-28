448ae1fbd2cb1d269f7d225993438cbfb209aaf8
Patricio Tena <tenapatricio@gmail.com>
2023-04-27 22:15:04 -0600

    Added subtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,9 +33,14 @@ const LandingPage = () => {
       </div>
 
       <div className='second-container'>
-        <p className="mb-1 ml-1 text-green font-mono text-[clamp(var(--fz-s),5vw,var(--fz-md))] font-normal">
-          Hey, my name is
-        </p>
+        <div className='flex'>
+          <p className="mb-1 ml-1 text-green font-mono text-[clamp(var(--fz-s),5vw,var(--fz-md))] font-normal">
+            01.
+          </p>
+          <p className="mb-1 ml-2 text-light font-mono text-[clamp(20px,6vw,30px)] font-extrabold">
+            About
+          </p>
+        </div>
       </div>
 
 
